3885bcdcae37749de808850aec990ee160062e28
KhumaloKat <162409577+KhumaloKat@users.noreply.github.com>
2024-06-21 10:38:13 +0200

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+CATEGORY_CHOICES =(
+    ('MN','Men'),
+    ('WM','Women'),
+    ('KD','Kid'),
+
+)
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image =models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.db.models import Count
+from django.shortcuts import render
+from django.views import View
+from . models import Product
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        titles = Product.objects.filter(category=val).values('title')
+        return render(request, 'app/category.html', locals()) #{'products': product, 'titles': titles})
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, "app/productdetail.html",locals())
