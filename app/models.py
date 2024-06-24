07e0c9f9368a70201302ea0b8e455145ea45ec22
KhumaloKat <162409577+KhumaloKat@users.noreply.github.com>
2024-06-24 10:40:41 +0200

    2nd commit

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     selling_price = models.FloatField()
     discount_price = models.FloatField()
+    discription = models.TextField()
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     product_image =models.ImageField(upload_to='product')
     def __str__(self):
